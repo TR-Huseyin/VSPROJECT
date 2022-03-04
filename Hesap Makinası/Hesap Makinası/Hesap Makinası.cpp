8e2de51d0fcc59424f70c2aa880b11dc97525bad
TR-Huseyin <84632917+TR-Huseyin@users.noreply.github.com>
2022-03-04 20:33:42 +0300

    Add files via upload

---
+++ Hesap Makinası/Hesap Makinası/Hesap Makinası.cpp
@@ -0,0 +1,20 @@
+﻿//by HÜSEYİN ÖZDEMİR
+#include <iostream>
+using namespace std;
+int main() {
+	double x, y, w = 0;
+	char z;
+	cout << "Lutfen yapacaginiz islemi secin" << endl << "(+)" << "(-)" << "(*)" << "(/)";
+	cin >> z;
+	cout << "Ilk veriyi giriniz";
+	cin >> x;
+	cout << "Ikinci degeri giriniz";
+	cin >> y;
+	if (z == 43) { w = x + y; }
+	if (z == 45) { w = x - y; }
+	if (z == 42) { w = x * y; }
+	if (z == 47) { w = x / y; }
+	if (w != 0) { cout << w; }
+	else { cout << "Hata"; }
+}
+//by HÜSEYİN ÖZDEMİR
